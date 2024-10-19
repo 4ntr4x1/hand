1f6d82a5720a807be715e0e8e0a2c23ffe7db8ab
4ntr4x1 <cris.p.lopez@gmail.com>
2024-10-19 01:32:32 -0700

    Initial Commit

---
+++ hand/handApp.swift
@@ -0,0 +1,17 @@
+//
+//  handApp.swift
+//  hand
+//
+//  Created by Chris Chains on 19/10/24.
+//
+
+import SwiftUI
+
+@main
+struct handApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ handTests/handTests.swift
@@ -0,0 +1,16 @@
+//
+//  handTests.swift
+//  handTests
+//
+//  Created by Chris Chains on 19/10/24.
+//
+
+import Testing
+
+struct handTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
